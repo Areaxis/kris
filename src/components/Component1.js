828cea2c73f98cd6f417c07764a372749a34a6c4
Areaxis <67699984+Areaxis@users.noreply.github.com>
2022-06-09 01:17:40 +0530

    Initial commit

---
+++ src/components/Component1.js
@@ -0,0 +1,13 @@
+export default function Component1() {
+    return (
+        <>
+            <div style={{ height: "300px", width: "300px", color: "pink", backgroundColor: "black" }}>
+                Ttesting
+            </div>
+
+            <div className="test">
+                Hiiii
+            </div>
+        </>
+    )
+}
